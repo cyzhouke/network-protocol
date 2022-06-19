8b96704f56e69841984bfc2a7a2f68165438edd9
zhouke <15927443395@163.com>
2022-06-19 16:09:20 +0800

    conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,8 +31,8 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-     'recommonmark',
-     'sphinx_markdown_tables'
+     'sphinx_markdown_tables',
+     'recommonmark'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
